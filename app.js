ba081354be01430945b748a65c4eb2d112dccef3
Tajtesz <tajtib96@gmail.com>
2017-09-29 07:49:31 +0200

    angular-app mappa, benne app.js
    angular install
    app.css css mappába áthelyezve

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use( function(req,res,next){
 });
 
 app.use( express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 app.use('/api',routes);
 
