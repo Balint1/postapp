6bf8648b2be45418acf47ce06517c00abaef3f97
barthabalint <bartha.balint@gmail.com>
2017-09-29 15:10:41 +0200

    Console.log-ok kiszedése

--- api/controllers/mails.controllers.js
+++ api/controllers/mails.controllers.js
@@ -1,14 +1,12 @@
 var mailData = require('../data/mails.json');
 
 module.exports.mailsGetAll = function(req,res){
-    console.log('GET the hotels');
     res
     .status(200)
     .json(mailData);
 };
 module.exports.mailsGetOne = function(req,res){
     var mailId = req.params.mailId;
-    console.log('GET mailId', mailId);
     var thisMail = mailData[mailId];
     res
     .status(200)
